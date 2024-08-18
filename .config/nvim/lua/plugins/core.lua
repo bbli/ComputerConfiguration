c6e217650ff9e5d5d7d029ab61ef7d722813a9bb
Benson Li <bensonbinbinli@gmail.com>
2024-08-17 21:06:31 -0700

    add edge colorscheme

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,13 +1,13 @@
 return {
-  --   -- add gruvbox
-  --   { "sainnhe/edge" },
-  --
-  --   -- Configure LazyVim to load gruvbox
-  --   {
-  --
-  --     "LazyVim/LazyVim",
-  --     opts = {
-  --       colorscheme = "edge",
-  --     },
-  --   }
+  -- add gruvbox test
+  { "sainnhe/edge" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "edge",
+    },
+  }
 }
