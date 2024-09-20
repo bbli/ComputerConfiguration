b0d35a0252dce15ffea6d4779ba937af71669e18
Benson Li <bensonbinbinli@gmail.com>
2024-09-19 19:23:50 -0700

    replaced lazyvim telescope config with my own

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,40 +1,41 @@
 local actions = require("telescope.actions")
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      --      file_ignore_patterns = {'build'},
-      mappings = {
-        n = {
-          ["q"] = actions.close,
-          ["<C-f>"] = actions.preview_scrolling_up,
-          ["<C-b>"] = actions.preview_scrolling_down,
-        },
-        i = {
-          ["<C-g>"] = actions.close,
-          ["<C-c>"] = actions.close,
-
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-          ["<C-l>"] = actions.smart_send_to_qflist + actions.open_qflist,
-          ["<C-h>"] = actions.complete_tag,
-          ["<C-u>"] = false,
-          ["<C-d>"] = false,
-          ["<C-b>"] = actions.preview_scrolling_up,
-          ["<C-f>"] = actions.preview_scrolling_down,
-        },
-      },
-      wrap_results = true,
-      layout_config = {
-        preview_width = 75,
-      },
-    },
-  },
   config = function()
     vim.keymap.del("n", "<leader>fr")
     vim.keymap.del("n", "<leader>ff")
     vim.keymap.del("n", "<leader><leader>")
+    require("telescope").setup({
+      defaults = {
+        --      file_ignore_patterns = {'build'},
+        mappings = {
+          n = {
+            ["q"] = actions.close,
+            ["<C-f>"] = actions.preview_scrolling_up,
+            ["<C-b>"] = actions.preview_scrolling_down,
+          },
+          i = {
+            ["<C-g>"] = actions.close,
+            ["<C-c>"] = actions.close,
+
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-l>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            ["<C-h>"] = actions.complete_tag,
+            ["<C-u>"] = false,
+            ["<C-d>"] = false,
+            ["<C-b>"] = actions.preview_scrolling_up,
+            ["<C-f>"] = actions.preview_scrolling_down,
+          },
+        },
+        wrap_results = true,
+        layout_config = {
+          preview_width = 75,
+        },
+      },
+    })
   end,
+
   keys = {
     { "<leader>oo", "<cmd>Telescope git_files<CR>" },
     {
