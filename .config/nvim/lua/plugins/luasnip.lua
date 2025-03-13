ce145df7ec2bbbcbd5e062c71f7e67907735daf2
Benson Li <bensonbinbinli@gmail.com>
2025-03-13 13:12:59 -0700

    FUTURE in org mode is now greyed out

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -98,7 +98,7 @@ ls.add_snippets(nil, {
   },
   cpp = {
     snip("sd", fmt([[// **************** {} ****************]], i(1, ""))),
-    snip("dd", fmt([[PS_DIAG_INFO(d_,"BENSON_DEBUG:: {}:{}",);]], { i(1, ""), i(2, "") })),
+    snip("dd", fmt([[PS_DIAG_INFO(d_,"BENSON_DEBUG:: {}:{}");]], { i(1, ""), i(2, "") })),
     snip("dv", fmt([[PS_DIAG_INFO(d_,"BENSON_DEBUG  {}:{}=%%",{});]], { i(1, ""), i(2, ""), rep(2) })),
   },
 })
