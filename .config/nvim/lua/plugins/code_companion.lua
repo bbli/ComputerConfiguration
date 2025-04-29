1b2a07d7d803e0ccfa193d4913d0b5a46d41dbf3
Benson Li <bensonbinbinli@gmail.com>
2025-04-28 21:27:56 -0700

    add VectorCode. Will need to manually init + vectorize

--- .config/nvim/lua/plugins/code_companion.lua
+++ .config/nvim/lua/plugins/code_companion.lua
@@ -4,6 +4,14 @@ return {
     strategies = {
       chat = {
         adaptor = "copilot",
+        tools = {
+          vectorcode = {
+            description = "Run VectorCode to retrieve the project context.",
+            callback = function()
+              return require("vectorcode.integrations").codecompanion.chat.make_tool()
+            end,
+          },
+        },
       },
       inline = {
         adaptor = "copilot",
@@ -15,6 +23,10 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown", "codecompanion" },
+      },
       "j-hui/fidget.nvim",
       {
         "Davidyz/VectorCode",
