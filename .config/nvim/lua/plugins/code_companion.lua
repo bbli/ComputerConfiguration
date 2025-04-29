679cae664e188436d578441c16254a49076759bd
Benson Li <bensonbinbinli@gmail.com>
2025-04-28 19:24:58 -0700

    removed aider.lua + start modifying code_companion.lua -> Chat is working on this commit

--- .config/nvim/lua/plugins/code_companion.lua
+++ .config/nvim/lua/plugins/code_companion.lua
@@ -1,10 +1,35 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    strategies = {
+      chat = {
+        adaptor = "copilot",
+      },
+      inline = {
+        adaptor = "copilot",
+      },
+      cmd = {
+        adaptor = "copilot",
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "j-hui/fidget.nvim",
+      {
+        "Davidyz/VectorCode",
+        version = "*",
+        build = "pipx upgrade vectorcode",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+      -- { "echasnovski/mini.pick", config = true },
+      -- { "ibhagwan/fzf-lua", config = true },
     },
+    keys = {
+      { "<leader>at", ":CodeCompanionChat Toggle<cr>", desc = "Toggle CodeCompanion" },
+    },
+    config = function()
+      require("codecompanion").setup({})
+    end,
   },
 }
