c85ce3331cbc318881e9867c1c8c2ecaa005f51c
Benson Li <bensonbinbinli@gmail.com>
2025-05-05 03:07:53 +0000

    removed diffview dependency so it won't mess with neogit keybindings

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -81,8 +81,8 @@ return {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim",
-      "ibhagwan/fzf-lua",              -- optional
+      --"sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua", -- optional
     },
     config = true,
     opts = {
